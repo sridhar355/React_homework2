3fae1d78075c6288a0e874a6145461fd7322ea07
sridhar <sridhar.meesala2000@gmail.com>
2024-05-29 23:44:47 +0530

    homework2 created

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='main'>
+      <h1>Topics Covered</h1>
+      <p>The following is a list of all the topics we cover in th MDN learning area.</p>
+      <a href="">Getting started with the web</a>
+      <p>Provides a practical introduction to web development for complete beginners.</p>
+      <a href="">HTML - Structuring the web</a>
+      <p>HTML is the language that we use to structure the different parts of out content and define what their meaning or purpose is. This topic teaches HTML in detail</p>
+      <a href="">CSS - Styling the web</a>
+      <p>CSS is the language that we use to control our web content&apos;s style and layout, as well as adding behaviour like animation. This topic provides comprehensive coverage of CSS</p>
+    </div>
+  )
+}
+
+export default App
